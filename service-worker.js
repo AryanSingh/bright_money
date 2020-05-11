9058351a2993f5755b3cba7b7f97e3b3066a7e76
aryan <aryan.iitgn@gmail.com>
2020-05-11 13:40:34 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AryanSingh/bright_money/precache-manifest.cf10f0d2f23c708aa50ae0f08edcdb83.js"
+  "/bright_money/precache-manifest.c29b4677e5e4dfdb9f8f2058695ae1a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AryanSingh/bright_money/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bright_money/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
