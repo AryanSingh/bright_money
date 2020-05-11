f3ac0c0b593966b91f67a50dec9dad71764e8aa9
aryan <aryan.iitgn@gmail.com>
2020-05-11 14:16:32 +0530

    Updates

---
+++ precache-manifest.1e8f569754fd542fb2b3b885155116ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e268c854145a54e4277a4c1d9ccba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e570e6d7699a277e53b",
+    "url": "/static/css/main.674a239e.chunk.css"
+  },
+  {
+    "revision": "4a1c731d0ab70109e2e3",
+    "url": "/static/js/2.deb11fc5.chunk.js"
+  },
+  {
+    "revision": "0c8d3b4e884b06928e2ac3e8ef3e55c2",
+    "url": "/static/js/2.deb11fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e570e6d7699a277e53b",
+    "url": "/static/js/main.b5f12d59.chunk.js"
+  },
+  {
+    "revision": "3362d1a1484c93731d67",
+    "url": "/static/js/runtime-main.33e916ee.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
